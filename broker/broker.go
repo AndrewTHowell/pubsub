93f8995dc22eafa696f027ed8ed5ac5446dc848a
Andrew Howell <andrewthomashowell@gmail.com>
2025-06-11 00:25:58 +0100

    Add errTopicNotFound

--- broker/broker.go
+++ broker/broker.go
@@ -1,5 +1,15 @@
 package broker
 
+import "fmt"
+
+type errTopicNotFound struct {
+	topic string
+}
+
+func (e errTopicNotFound) Error() string {
+	return fmt.Sprintf("topic %q not found", e.topic)
+}
+
 func New(topics ...string) Broker {
 	return Broker{messagesByTopic: make(map[string][]Message, len(topics))}
 }
@@ -9,10 +19,16 @@ type Broker struct {
 }
 
 func (b Broker) Publish(topic string, message Message) error {
+	if _, ok := b.messagesByTopic[topic]; !ok {
+		return errTopicNotFound{topic: topic}
+	}
 	return nil
 }
 
 func (b Broker) Subscribe(topic string) (<-chan Message, error) {
+	if _, ok := b.messagesByTopic[topic]; !ok {
+		return nil, errTopicNotFound{topic: topic}
+	}
 	return nil, nil
 }
 
