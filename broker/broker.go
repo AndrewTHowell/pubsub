aafe5c35524eea94dec2010c79865bc761bc186e
Andrew Howell <andrewthomashowell@gmail.com>
2025-06-22 23:40:18 +0100

    Concurrency control

--- broker/broker.go
+++ broker/broker.go
@@ -3,6 +3,7 @@ package broker
 import (
 	"fmt"
 	"log"
+	"sync"
 )
 
 type errTopicNotFound struct {
@@ -13,14 +14,14 @@ func (e errTopicNotFound) Error() string {
 	return fmt.Sprintf("topic %q not found", e.topic)
 }
 
-func New(topics ...string) Broker {
+func New(topics ...string) *Broker {
 	messagesByTopic := make(map[string]*[]Message, len(topics))
 	offsetByGroupByTopic := make(map[string]map[string]int, len(topics))
 	for _, topic := range topics {
 		messagesByTopic[topic] = &[]Message{}
 		offsetByGroupByTopic[topic] = map[string]int{}
 	}
-	return Broker{
+	return &Broker{
 		messagesByTopic:      messagesByTopic,
 		offsetByGroupByTopic: offsetByGroupByTopic,
 	}
@@ -28,11 +29,15 @@ func New(topics ...string) Broker {
 
 // TODO: concurrency control.
 type Broker struct {
+	mutex                sync.RWMutex
 	messagesByTopic      map[string]*[]Message
 	offsetByGroupByTopic map[string]map[string]int
 }
 
-func (b Broker) Publish(topic string, message Message) error {
+func (b *Broker) Publish(topic string, message Message) error {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
 	messages, ok := b.messagesByTopic[topic]
 	if !ok {
 		return errTopicNotFound{topic: topic}
@@ -42,7 +47,10 @@ func (b Broker) Publish(topic string, message Message) error {
 	return nil
 }
 
-func (b Broker) Subscribe(topic, group string, maxBufferSize int) (Poller, error) {
+func (b *Broker) Subscribe(topic, group string, maxBufferSize int) (Poller, error) {
+	b.mutex.RLock()
+	defer b.mutex.RUnlock()
+
 	if _, ok := b.messagesByTopic[topic]; !ok {
 		// Check at subscribe time.
 		return nil, errTopicNotFound{topic: topic}
@@ -54,8 +62,11 @@ func (b Broker) Subscribe(topic, group string, maxBufferSize int) (Poller, error
 	return b.poller(topic, group, maxBufferSize), nil
 }
 
-func (b Broker) poller(topic, group string, maxBufferSize int) Poller {
+func (b *Broker) poller(topic, group string, maxBufferSize int) Poller {
 	return func() ([]Message, error) {
+		b.mutex.Lock()
+		defer b.mutex.Unlock()
+
 		// Caller guarantees that these keys are present.
 		messages := b.messagesByTopic[topic]
 		offsetByGroup := b.offsetByGroupByTopic[topic]
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"sync"
 
 	"pubsub/broker"
 )
@@ -15,6 +17,11 @@ func main() {
 	var pub Publisher = b
 	var sub Subscriber = b
 
+	simple(pub, sub)
+	//concurrent(pub, sub)
+}
+
+func simple(pub Publisher, sub Subscriber) {
 	poll, err := sub.Subscribe("animals.cats", "group-1", 10)
 	if err != nil {
 		log.Fatalf("Subscribing to 'animals.cats' topic: %v", err)
@@ -35,6 +42,49 @@ func main() {
 	}
 
 	log.Printf("Polled messages: %+v", messages)
+}
+
+func concurrent(pub Publisher, sub Subscriber) {
+	publishWG := sync.WaitGroup{}
+
+	publish := func(topic string, payload string) {
+		pub.Publish(topic, broker.Message{Payload: payload})
+		publishWG.Done()
+	}
+	for i := range 10 {
+		publishWG.Add(2)
+		go publish("animals.cats", fmt.Sprintf("cat-%d", i))
+		go publish("animals.dogs", fmt.Sprintf("dog-%d", i))
+	}
+
+	publishWG.Wait()
+	pollWG := sync.WaitGroup{}
+
+	poll := func(id int, topic, group string) {
+		poll, err := sub.Subscribe(topic, group, 10)
+		if err != nil {
+			log.Fatalf("Poller %q topic %q group %q subscribing: %v", fmt.Sprint(id), topic, group, err)
+		}
+
+		messages, err := poll()
+		if err != nil {
+			log.Fatalf("Poller %q topic %q group %q polling: %v", fmt.Sprint(id), topic, group, err)
+		}
+		log.Printf("Poller %q topic %q group %q polled: %+v", fmt.Sprint(id), topic, group, messages)
+
+		pollWG.Done()
+	}
+
+	id := 0
+	for i := range 3 {
+		pollWG.Add(3)
+		go poll(id, "animals.cats", fmt.Sprintf("group-%d", i))
+		go poll(id+1, "animals.cats", fmt.Sprintf("group-%d", i))
+		go poll(id+2, "animals.dogs", fmt.Sprintf("group-%d", i))
+		id += 3
+	}
+
+	pollWG.Wait()
 }
 
 type Publisher interface {
