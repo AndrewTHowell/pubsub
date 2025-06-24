7204db5294159b2035a5ea8acad724e9bb8bd15f
Andrew Howell <andrewthomashowell@gmail.com>
2025-06-24 23:38:13 +0100

    Load ports via config

--- broker/main.go
+++ broker/main.go
@@ -5,14 +5,31 @@ import (
 	"log"
 	"net"
 
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
 	"google.golang.org/grpc"
 
 	brokergrpc "pubsub/broker/grpc"
 	brokerpb "pubsub/broker/proto/broker"
 )
 
+type config struct {
+	Port int `koanf:"port"`
+}
+
+// Global koanf instance. Use "." as the key path delimiter. This can be "/" or any character.
+var k = koanf.New(".")
+
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 9123))
+	// Load YAML config.
+	if err := k.Load(file.Provider("broker/config.yml"), yaml.Parser()); err != nil {
+		log.Fatalf("loading config: %v", err)
+	}
+	var cfg config
+	k.Unmarshal("config", &cfg)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", cfg.Port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -20,7 +37,7 @@ func main() {
 	srv := grpc.NewServer()
 	brokerpb.RegisterBrokerServer(srv, brokergrpc.NewServer("animals.cats"))
 
-	log.Println("Starting Broker.")
+	log.Printf("Starting Broker, listening on port %d.\n", cfg.Port)
 	if srv.Serve(lis); err != nil {
 		log.Fatalf("server exited: %v", err)
 	}
--- go.mod
+++ go.mod
@@ -3,13 +3,22 @@ module pubsub
 go 1.24.4
 
 require (
+	github.com/knadh/koanf/parsers/yaml v1.0.0
+	github.com/knadh/koanf/providers/file v1.2.0
+	github.com/knadh/koanf/v2 v2.2.1
 	google.golang.org/grpc v1.73.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/knadh/koanf/maps v0.1.2 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	golang.org/x/net v0.38.0 // indirect
-	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
