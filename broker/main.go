911001a744a85a043cd52c450f71d619535b61bb
Andrew Howell <andrewthomashowell@gmail.com>
2025-07-13 10:41:05 +0100

    Logging verbosity

--- broker/main.go
+++ broker/main.go
@@ -16,8 +16,18 @@ import (
 )
 
 type Config struct {
-	Port   int      `koanf:"port"`
-	Topics []string `koanf:"topics"`
+	Logging LoggingConfig `koanf:"logging"`
+	Port    int           `koanf:"port"`
+	Topics  []Topic       `koanf:"topics"`
+}
+
+type LoggingConfig struct {
+	Verbosity string `koanf:"verbosity"`
+}
+
+type Topic struct {
+	Name               string `koanf:"name"`
+	NumberOfPartitions int    `koanf:"num_of_partitions"`
 }
 
 func main() {
@@ -26,6 +36,17 @@ func main() {
 		slog.Error("Parsing config", slog.Any("error", err))
 		os.Exit(1)
 	}
+
+	loggingLevel := slog.LevelInfo
+	switch cfg.Logging.Verbosity {
+	case "debug":
+		loggingLevel = slog.LevelDebug
+	default:
+		slog.Error("Invalid logging verbosity", slog.String("verbosity", cfg.Logging.Verbosity))
+		os.Exit(1)
+	}
+	slog.SetLogLoggerLevel(loggingLevel)
+
 	slog.Debug("Config loaded", slog.Any("config", cfg))
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", cfg.Port))
@@ -35,7 +56,11 @@ func main() {
 	}
 
 	srv := grpc.NewServer(grpc.ChainUnaryInterceptor(grpcerrors.UnaryServerInterceptor))
-	brokerpb.RegisterBrokerServer(srv, brokergrpc.NewServer(cfg.Topics...))
+	topics := []string{}
+	for _, t := range cfg.Topics {
+		topics = append(topics, t.Name)
+	}
+	brokerpb.RegisterBrokerServer(srv, brokergrpc.NewServer(topics...))
 
 	log.Printf("Starting Broker, listening on port %d.\n", cfg.Port)
 	if srv.Serve(lis); err != nil {
